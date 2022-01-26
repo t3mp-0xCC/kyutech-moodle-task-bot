b555b1f5cafb235fa3a21d6de0fcdfa5596f2fa5
t3mp-0xCC <40132263+t3mp-0xCC@users.noreply.github.com>
2022-01-27 05:27:39 +0900

    add requirements.txt

--- discord.py
+++ discord.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
+import discord
+""" import from moodle_api.py """
 from moodle_api import login, get_upcoming_tasks_as_text, get_upcoming_tasks, from_dict_to_set
+
+TOKEN = '*** PASTE YOUR DISCORD BOT TOKEN HERE ***'
+
+client = discord.Client()
