b5b970d9b10b1c4da1213da2cb7960d464e9c6ce
t3mp-0xCC <40132263+t3mp-0xCC@users.noreply.github.com>
2022-01-27 11:12:27 +0900

    update discord.py

--- discord.py
+++ discord.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python3
+from discord.ext import tasks
 import discord
-""" import from moodle_api.py """
+# import from moodle_api.py
 from moodle_api import login, get_upcoming_tasks_as_text, get_upcoming_tasks, from_dict_to_set
 
+
 TOKEN = '*** PASTE YOUR DISCORD BOT TOKEN HERE ***'
-moodle_id = '*** PASTE YOUR MOODLE ID HERE ***'
-moodle_passwd = '*** PASTE YOUR MOODLE PASSWORD HERE ***'
+CHANNEL_ID = '*** PASTE CHANNEL_ID ***'
+MOODLE_ID = '*** PASTE YOUR MOODLE ID HERE ***'
+MOODLE_PASSWORD = '*** PASTE YOUR MOODLE PASSWORD HERE ***'
+
 
 client = discord.Client()
+channel_sent = None
+
+@tasks.loop(minutes=180)
+async def check_moodle():
+    """ Check moodle tasks page """
+    login(MOODLE_ID, MOODLE_PASSWORD)
+    tasks = get_upcoming_tasks()
+    await channel_sent.send(tasks)
+
+@client.event
+async def on_ready():
+    global channel_sent
+    channel_sent = client.get_channel(CHANNEL_ID)
+    check_moodle.start()
+
+client.run(TOKEN)
