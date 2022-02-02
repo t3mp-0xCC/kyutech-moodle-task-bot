fcbeb987af03bde7ebed912a3e625f9919bd63fb
t3mp-0xCC <40132263+t3mp-0xCC@users.noreply.github.com>
2022-02-02 16:57:38 +0900

    tmp

--- db.py
+++ db.py
@@ -4,15 +4,29 @@ DB = 'main.db'
 
 def db_init():
     print("[+] databse init")
-    with sqlite3.connect(DB) as db:
-        cur = db.cursor()
+    with sqlite3.connect(DB) as conn:
+        cur = conn.cursor()
         cur.execute('CREATE TABLE IF NOT EXISTS schedule(time STRING)')
         cur.execute('SELECT COUNT(*) from schedule')
         result = cur.fetchall()
-        print(result)
         if result[0][0] == 0:
             print("[i] database is empty !")
             cur.execute('INSERT INTO schedule(time) values("8:00")')
-            db.commit()
+            cur.execute('INSERT INTO schedule(time) values("12:00")')
+            conn.commit()
 
         cur.close()
+    conn.close()
+
+
+def read_table(table):
+    """ Read databse table and return list """
+    conn = sqlite3.connect(DB)
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM {}'.format(table))
+    data = cur.fetchall()
+    print("[+] read table: {}".format(table))
+    print(data)
+    cur.close()
+    conn.close()
+    return data
--- scheduler.py
+++ scheduler.py
@@ -1,13 +1,14 @@
 # -*- coding:utf-8 -*-
 import datetime
+from db import read_table
+
+TABLE = 'schedule'
 
 
 def read_schedule(path):
-    """ Read schedule file(path) """
-    List =  open(path, 'r', encoding='utf-8').readlines()
-    print("[i] read_schedule from {}".format(path))
-    print('\n'.join(List))
-    return List
+    """ Read schedule from database """
+    schedule = read_table(TABLE)# list of tuple
+    return schedule
 
 
 def add_schedule(path, time):
@@ -71,17 +72,16 @@ def check_duplicate(path, val):
 
 
 def check_schedule(path):
-    """ Check schedule from the file(path) """
+    """ Compare  """
     now = datetime.datetime.now()
     print("[i] now: {}".format(now))
-    file = read_schedule(path)
+    schedule = read_schedule(path)
     for i in range(10):# check rug (check_every_ten_min)
-        time = (now - datetime.timedelta(minutes=i)).strftime('%H:%M')
-        if time[0] == '0':# remove first 0(ex. 08:00 -> 8:00)
-            time = time.replace(time[0], '', 1)
-
-        for schedule in file:
-            schedule = schedule.replace('\n','')
+        cmp_time = (now - datetime.timedelta(minutes=i)).strftime('%H:%M')
+        if cmp_time[0] == '0':# remove first 0(ex. 08:00 -> 8:00)
+            cmp_time = cmp_time.replace(cmp_time[0], '', 1)
+        
+        for schedule in times:
             print("[i] compare {} and {}".format(time, schedule))
             if schedule == time:
                 print("[+] schedule matched !")
